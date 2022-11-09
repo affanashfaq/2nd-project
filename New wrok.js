44302fb73c65efebf8670ed2ce5d77ece15964b1
unknown <affanashfaq786@gmail.com>
2022-11-09 08:12:44 -0800

    New Work Compelete

---
+++ New wrok.js
@@ -0,0 +1,12 @@
+var a = ["a","b","c","d","e"]
+a.pop()
+a.push('affan');
+a.shift();
+a.unshift("affan");
+var b = a.slice(0,5)
+var b = a.splice(2,2)
+console.log(a);
+console.log(b);
+for(var i = 0; i  < 10; i++){
+      console.log("5 *" + (i + 1) + "=" + 5 * ( i + 1));
+} 
